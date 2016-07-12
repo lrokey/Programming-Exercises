04bd9667960cbba3c2b4ab92eec1888c236ae0ca
lrokey <larokey@mail.fhsu.edu>
2016-07-12 10:44:11 -0500

    Create water.c

---
+++ CS50/pset1/water.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  printf("How many minutes is your shower?\n");
+  int lengthOfShower = GetInt();
+  int bottlesOfWater = lengthOfShower * 12;
+  printf("Your %i minute-shower is using %i bottles of water\n", lengthOfShower, bottlesOfWater);
+}
