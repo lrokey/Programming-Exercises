f88f0c0ea2aadc6e1aaecb82b97f0fb97badc398
lrokey <larokey@mail.fhsu.edu>
2016-07-12 10:54:45 -0500

    Create greedy1.c

---
+++ CS50/pset1/greedy1.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    printf("O hai! How much change is owed?\n");
+    int minCoins = 0;
+    float change = GetFloat();
+    while (change < 0){
+        printf("Retry: ");
+        change = GetFloat();
+    }
+    //convert dollars to cents
+    int changeInCents = change*100;
+    //printf("change in cents is %i\n", changeInCents);
+    while (changeInCents > 0){
+        if (changeInCents >= 25){
+            changeInCents -= 25;
+            minCoins++;
+        }else if(changeInCents < 25 && changeInCents >= 10){
+            changeInCents -= 10;
+            minCoins++;
+        }else if(changeInCents < 10 && changeInCents >= 5){
+            changeInCents -= 5;
+            minCoins++;
+        }else{
+            changeInCents -= 1;
+            minCoins++;
+        }
+    }
+    printf("%i\n", minCoins);
+}
